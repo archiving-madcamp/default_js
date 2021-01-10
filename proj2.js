92422f2adf32e0a3b9a66beef7505909a262225a
hyuna <kiipo0623@gmail.com>
2021-01-10 08:53:13 +0000

    mongoose

--- proj2.js
+++ proj2.js
@@ -1,8 +1,9 @@
 const express = require('express');
+var mongoose = require('mongoose');
 
-var MongoClient = require('mongodb').MongoClient;
 
-var url = "mongodb://localhost:27017/";
+
+var url = "mongodb://localhost:27017/testdb";
 
 var test_id;
 var test_name;
@@ -46,8 +47,46 @@ app.post('/post', (req, res)=>{
     req.on('end', ()=>{
         test_id = inputData.user_id;    //안드로이드에서 키값
         test_name = inputData.name;
-        console.log("user_id"+inputData.user_id+", name:"+inputData.name);
+        
+        //한번은 올라가 지는데 두번은 안되는 문제
+        mongoose.connect(url, {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+        })
+        .then(()=>{
+            console.log('connected')
+        })
+        .catch((err)=>{
+            console.log(err)
+        })
 
+        const UserSchema = new mongoose.Schema({
+            user_id : String,
+            name: String
+        })
+
+        //한번만 만들어야되는데 컬렉션을 여러번 만들어서 오류나는듯함
+        var User = mongoose.model("hi", UserSchema)
+
+        var me = new User({
+            user_id: test_id,
+            name: test_name
+        })
+
+        me.save()
+        .then(() => {
+            console.log(me);
+        })
+        .catch((err) =>{
+            console.log(err);
+        })
+
+
+      
+
+    })
+
+/*
         MongoClient.connect(url, function(err, db) {
             if (err) throw err;
 
@@ -60,11 +99,12 @@ app.post('/post', (req, res)=>{
             });
           });
     });
-    
+*/
     res.write("OK!");
     res.end();
 });
 
+
 app.listen(3000, () => {
     console.log('Example app listeneing on post 3000');
-});
+})
